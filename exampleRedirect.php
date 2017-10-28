3e44e72500b6e111d56cc261ea1ff77ad265b811
Miic <micruz129@gmail.com>
2017-10-27 20:06:19 -0700

    addedExampleRedirect

---
+++ exampleRedirect.php
@@ -0,0 +1,54 @@
+<?php
+
+#Removed appending PHP code for demo for security.
+
+echo "<!DOCTYPE HTML>\n"; 
+echo "<!--\n"; 
+echo "        Massively by HTML5 UP\n"; 
+echo "        html5up.net | @ajlkn\n"; 
+echo "        Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)\n"; 
+echo "-->\n"; 
+echo "<html>\n"; 
+echo "        <head>\n"; 
+echo "                <title>ElyssiaMC - VPN Detected!</title>\n"; 
+echo "                <meta charset=\"utf-8\" />\n"; 
+echo "                <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, user-scalable=no\" />\n"; 
+echo "                <link rel=\"stylesheet\" href=\"assets/css/main.css\" />\n"; 
+echo "                <noscript><link rel=\"stylesheet\" href=\"assets/css/noscript.css\" /></noscript>\n"; 
+echo "        </head>\n"; 
+echo "        <body class=\"is-loading\">\n"; 
+echo "\n"; 
+echo "                <!-- Wrapper -->\n"; 
+echo "                        <div id=\"wrapper\" class=\"fade-in\">\n"; 
+echo "\n"; 
+echo "                                <!-- Intro -->\n"; 
+echo "                                        <div id=\"intro\">\n"; 
+echo "                                                <h1>VPN detected!</h1>\n"; 
+echo "                                                <p><!--[-->Please turn off your VPN to access our site.<!--]--></p>\n"; 
+echo "                                        </div>\n"; 
+echo "\n"; 
+echo "                                <!-- Header -->\n"; 
+echo "                                        <header id=\"header\">\n"; 
+echo "                                                <a href=\"index.html\" class=\"logo\">Massively</a>\n"; 
+echo "                                        </header>\n"; 
+echo "\n"; 
+echo "                                <!-- Nav -->\n"; 
+echo "                                        <nav id=\"nav\">\n"; 
+echo "                                                <ul class=\"links\">\n"; 
+echo "                                                        <li><a href=\"https://elyssiamc.com/\">Main Website</a></li>\n"; 
+echo "                                                        <li><a href=\"http://forum.elyssiamc.com/\">Forums</a></li>\n"; 
+echo "                                                </ul>\n"; 
+echo "                                        </nav>\n"; 
+echo "\n"; 
+echo "                        </div>\n"; 
+echo "\n"; 
+echo "                <!-- Scripts -->\n"; 
+echo "                        <script src=\"assets/js/jquery.min.js\"></script>\n"; 
+echo "                        <script src=\"assets/js/jquery.scrolly.min.js\"></script>\n"; 
+echo "                        <script src=\"assets/js/skel.min.js\"></script>\n"; 
+echo "                        <script src=\"assets/js/util.js\"></script>\n"; 
+echo "                        <script src=\"assets/js/main.js\"></script>\n"; 
+echo "\n"; 
+echo "        </body>\n"; 
+echo "</html>\n";
+?>
